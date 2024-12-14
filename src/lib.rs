300e32f66007cd5c0b586a2e929d523bd309bc3f
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2024-12-14 17:15:53 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -363,11 +363,22 @@ mod tests {
         write_root(&file_name, &write_data)?;
         assert_file_permission(&file_name, true);
         command_try("chmod", ["700", &file_name])?;
-
+        
         let result = read_user(&file_name);
         print!("{:?}", result);
         assert!(result.is_err());
+
         let result = read_root(&file_name);
+        assert!(result.is_ok());
+        let read_data = result.unwrap();
+        assert_eq!(write_data, read_data);
+
+        let result = read_sudo(&file_name);
+        assert!(result.is_ok());
+        let read_data = result.unwrap();
+        assert_eq!(write_data, read_data);
+
+        let result = read_try(&file_name);
         assert!(result.is_ok());
         let read_data = result.unwrap();
         assert_eq!(write_data, read_data);
